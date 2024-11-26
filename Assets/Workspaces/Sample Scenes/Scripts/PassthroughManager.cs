151c9406240cabd6e6b427e9fb111d027745f577
Argo <dargo@mcghi.mcg.edu>
2024-11-26 10:58:34 -0500

    This is a pre-pull do not merge

--- Assets/Workspaces/Sample Scenes/Scripts/PassthroughManager.cs
+++ Assets/Workspaces/Sample Scenes/Scripts/PassthroughManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using Meta.XR.MultiplayerBlocks.NGO;
 using UnityEngine;
 
-public class PassthroughManager : MonoBehaviour
-{
+public class PassthroughManager : MonoBehaviour {
     public static PassthroughManager Singleton { get; private set; }
 
     private bool passthroughOn = false;
@@ -13,21 +12,7 @@ public class PassthroughManager : MonoBehaviour
     
     [SerializeField] private OVRPassthroughLayer layer;
     [SerializeField] private Color defaultBackground;
-
-    [SerializeField] private GameObject[] selectiveElements;
-    [SerializeField] private GameObject[] disableOnPassthrough;
-
-    [SerializeField] private bool toggle;
-    
     [SerializeField] private int hideOnPassthrough;
-    
-    private bool toggleSet;
-    
-    // BEGIN Example Code Only
-    [Space] 
-    [SerializeField] private GameObject passthroughButton;
-    [SerializeField] private GameObject selectivePassthroughButton;
-    // END Example Code Only
     
     void Awake() {
         if (Singleton != null && Singleton != this) Destroy(this.gameObject);
@@ -35,73 +20,23 @@ public class PassthroughManager : MonoBehaviour
         
         rig = GameManager.Singleton.ovrCameraRig;
     }
-
-    void Update()
-    {
-        if (toggle != toggleSet)
-        {
-            Toggle();
-            toggleSet = toggle;
-        }
-    }
-
-    public void Toggle()
-    
-    {
+    public void Toggle() {
         Camera centerCamera = rig.centerEyeAnchor.GetComponent<Camera>();
-        
-        if (passthroughOn)
-        {
+        if (passthroughOn) {
             layer.enabled = false;
             centerCamera.clearFlags = CameraClearFlags.Skybox;
             centerCamera.backgroundColor = defaultBackground; 
             
-            // disable hide on passthrough
-        }
-        else
-        {
+            // enable the things in hide on passthrough
+        } else {
             layer.enabled = true;
             
             centerCamera.clearFlags = CameraClearFlags.SolidColor;
             centerCamera.backgroundColor = Color.clear;
             
-            //enable hide on passthrough 
-            rig.centerEyeAnchor.GetComponent<Camera>().cullingMask &= ~(1 << hideOnPassthrough);
+            // disable the things in hide on passthrough 
         }
 
         passthroughOn = !passthroughOn;
-        
-        // if Normal Passthrough On, Disable selectivePassthrough items
-        selectivePassthroughButton.SetActive(!passthroughOn); 
-    }
-
-    public void SelectiveToggle()
-    {
-        Camera centerCamera = rig.centerEyeAnchor.GetComponent<Camera>();
-        
-        if (selectivePassthroughOn)
-        {
-            layer.enabled = false;
-            
-            centerCamera.backgroundColor = defaultBackground; 
-            
-        }
-        else
-        {
-            layer.enabled = true;
-            
-            centerCamera.backgroundColor = Color.clear;
-            
-            foreach (var element in selectiveElements)
-            {
-                element.SetActive(true);
-                element.GetComponent<TransferOwnershipNGO>().TransferOwnershipToLocalPlayer();
-            }
-        }
-        
-        selectivePassthroughOn = !selectivePassthroughOn;
-
-        // if Selective Passthrough On, Disable passthrough items
-        passthroughButton.SetActive(!selectivePassthroughOn); 
     }
 }
