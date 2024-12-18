1e94425607c23291bb73f71cf517c0f0378f1a6a
Argo <dargo@mcghi.mcg.edu>
2024-12-17 19:50:32 -0500

    lots of stuffffff

--- Assets/Workspaces/Erkin/Inverse Follow Player.cs
+++ Assets/Workspaces/Erkin/Inverse Follow Player.cs
@@ -4,7 +4,6 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class InverseFollowPlayer : NetworkBehaviour {
-    private bool followPlayer = false;
     private Transform minimap;
     private Transform realMap;
 
@@ -12,10 +11,13 @@ public class InverseFollowPlayer : NetworkBehaviour {
     
     public NetworkVariable<bool> _mapPos = new(writePerm: NetworkVariableWritePermission.Server);
     public NetworkVariable<bool> _truePos = new(writePerm: NetworkVariableWritePermission.Server);
-
+    public NetworkVariable<bool> isPlayerAR = new(writePerm: NetworkVariableWritePermission.Owner);
+    
     private InverseFollowPlayer _otherPlayerPrefab;
 
     private GameObject line;
+
+    private GameObject newOriginPos;
     
     void Start() {
         minimap = GameManager.Singleton.minimap;
@@ -25,17 +27,15 @@ public class InverseFollowPlayer : NetworkBehaviour {
             if (other == this) continue;
             _otherPlayerPrefab = other;
         }
+
+        if (_truePos.Value) {
+            newOriginPos = new GameObject("originPos");
+            newOriginPos.transform.position = GameManager.Singleton.mainCamera.transform.position;
+        }
     }
 
 
     void CalculateMinimapInversePosition() {
-        /*Vector3 playerWorldPosition = GameManager.Singleton.mainCamera.transform.position;
-
-        Vector3 relativePos = realMap.TransformPoint(playerWorldPosition);
-        relativePos = Vector3.Scale(relativePos, new Vector3(0.01f, 0.01f, 0.01f));
-
-        transform.position = minimap.TransformPoint(relativePos);*/
-        
         Vector3 playerOffset = GameManager.Singleton.mainCamera.transform.position - realMap.transform.position;
 
         playerOffset.x *= 0.001f;
@@ -48,23 +48,13 @@ public class InverseFollowPlayer : NetworkBehaviour {
         transform.position = adjustedPos;
         
         transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        
+        DrawConnections(transform, _otherPlayerPrefab.transform);
     }
 
     void CalculateTrueInversePosition() {
-        Vector3 playerPosition = GameManager.Singleton.mainCamera.transform.position;
-
-        // Calculate the offset from the player to the origin
-        Vector3 playerOffset = origin.Value - playerPosition;
-
-        // Set the object position relative to the origin and offset
-        transform.position = playerPosition + playerOffset;
-
-        // Log the origin for debugging purposes
-        Debug.Log(origin.Value);
-
-        // Adjust the position by locking the Y-value to the minimap's Y
-        Vector3 adjustedPos = new Vector3(transform.position.x, minimap.position.y, transform.position.z);
-        transform.position = adjustedPos;
+        Vector3 offset = GameManager.Singleton.mainCamera.transform.position - newOriginPos.transform.position;
+        transform.position = origin.Value + offset;
     }
 
     void DrawConnections(Transform start, Transform end) {       
@@ -76,15 +66,26 @@ public class InverseFollowPlayer : NetworkBehaviour {
         
         line.transform.position = midPoint;            
         line.transform.rotation = Quaternion.LookRotation(direction);            
-        line.transform.localScale = new Vector3(0.2f, 0.2f, direction.magnitude);                        
-        // connector.GetComponent<MeshRenderer>().material.SetColor("_Color", node.GetRouteColor());
-    } 
+        line.transform.localScale = new Vector3(0.002f, 0.002f, direction.magnitude);                     
+    }
+
+    [ServerRpc(RequireOwnership = true)]
+    public void CleanUpServerRpc(ServerRpcParams serverRpcParams = default) {
+        NetworkObject.Despawn(gameObject);
+    }
+
+    void LateUpdate() {
+        if(IsOwner) isPlayerAR.Value = PassthroughManager.Singleton.passthroughOn;
+        
+        if (isPlayerAR.Value) {
+            if(line != null) Destroy(line);
+            if (IsOwner) CleanUpServerRpc();
+        }
+    }
         
     void Update() {
         if (_mapPos.Value) CalculateMinimapInversePosition();
         if (_truePos.Value) CalculateTrueInversePosition();
-        
-        DrawConnections(transform, _otherPlayerPrefab.transform);
     }
     
 }
