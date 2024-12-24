5f8a1b5bbfd734be14afcfb90228c231500050aa
Khoroshi Eisalou <ekhoroshieisalo@mcghi.mcg.edu>
2024-12-24 16:13:55 -0500

    Christmas Time

---
+++ Assets/Workspaces/Erkin/VisualEffects.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualEffects : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> trees;
+    private float timer = 0f;
+    public float activationDelay = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        foreach (GameObject tree in trees)
+        {
+            tree.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateTrees() {
+        int rand = Random.Range(0, trees.Count);
+
+        timer += Time.deltaTime;
+
+        if (timer > activationDelay) {
+            trees[rand].SetActive(true);    
+        }
+    }
+}
